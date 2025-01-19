1ba4df076e91b562c26ca20541d8afda0c04e0e6
Denis <porodontezz@gmail.com>
2025-01-19 17:20:54 +0500

    program_ver_1

---
+++ save_notes_to_file.py
@@ -0,0 +1,23 @@
+import datetime
+
+dt = datetime.datetime.now()
+formatted = dt.strftime('%d.%m.%Y')
+
+def save_notes_to_file():
+    
+    my_file = open('filename.txt', 'w+', encoding='utf-8')
+    user_name = input("Введите имя пользователя: ")
+    title = input("Введите заголовок заметки: ")
+    content = input("Введите описание заметки: ")
+    status = input("Введите статус заметки (Новая, Активна, Завершена): ")
+    issue_date = input("Введите дату истечения заметки (день.месяц.год): ")
+
+    my_file.write(f"Имя пользователя: {user_name}\n")
+    my_file.write(f"Заголовок: {title}\n")
+    my_file.write(f"Описание заметки: {content}\n")
+    my_file.write(f"Статус заметки: {status}\n")
+    my_file.write(f"Дата создания заметки: {formatted}\n")
+    my_file.write(f"Истечение заметки: {issue_date}\n")
+    my_file.close()
+
+save_notes_to_file()
